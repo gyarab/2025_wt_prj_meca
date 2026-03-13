e560b716cbbfe26c726ad00783947776b6aa9a78
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-03-13 10:19:06 +0100

    Update admin.py

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Player,Game
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display =['whitePlayer','rating']
+    pass
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin);
+    pass
 
 # Register your models here.
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+   
+class Player(models.Model): 
+   # id
+   name = models.CharField(max_length=255)
+   surname = models.CharField(max_length=255)
+   rating = models.PositiveSmallIntegerField(blank = True, null = True)
+   birth_year = models.PositiveSmallIntegerField(blank = True, null = True)
+
+class Game(models.Model):
+   # id
+   white_player = models.ForeignKey("Player", on_delete= models.SET_NULL, null= True)
+   black_player = models.ForeignKey("Player", on_delete= models.SET_NULL, null = True)
+   tournament_id = models.ForeignKey("Tournament")
+   opening_id = models.ForeignKey("Opening")
+   release_date = models.DateTimeField
+   result = models.CharField("1-0","1/2-1/2", "0-1")

802c4f228a21f09da69c9fabbb49dd91d5ca3896
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-05-09 20:33:18 +0200

    Update urls.py

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from app import views
+from app.api import api
 
 urlpatterns = [
+    path("api/", api.urls),
     path('admin/', admin.site.urls),
     path('', views.render_homepage, name ="homepage"),
  #  path('players/', views.render_players, name = "players"),

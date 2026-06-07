9f9a4f4f464c77a53a6b170bfc7433ce46fc37b0
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-06-07 15:48:32 +0200

    Update GameList.vue

--- frontend/src/views/GameDetail.vue
+++ frontend/src/views/GameDetail.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const game = ref(null)
+
+async function load() {
+    const url = new URL(`/api/game/${route.params.id}`, window.location.origin)
+    const res = await fetch(url)
+    if (!res.ok) throw new Error(`HTTP returned ${res.status}`)
+    game.value = await res.json()
+}
+
+onMounted(load)
+</script>
+
 <template>
     <h2>Game Detail</h2>
+    <div v-if="game">
+        <h3>{{ game.title }}</h3>
+        <p>{{ game.developer.name }} ({{ game.developer.founded_year }})</p>
+        <img :src="game.poster_url" style="max-height: 100px">
+    </div>
 </template>
--- frontend/src/views/GameList.vue
+++ frontend/src/views/GameList.vue
@@ -1,3 +1,33 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const games = ref([])
+const router = useRouter()
+
+async function load() {
+    const url = new URL('/api/game', window.location.origin)
+    const res = await fetch(url)
+    if (!res.ok) throw new Error(`HTTP returned ${res.status}`)
+    const data = await res.json()
+    games.value = data.results
+
+}
+
+function selectGame(game) {
+    router.push(`/game/${game.id}`)
+}
+
+onMounted(load)
+</script>
+
 <template>
-    <h2>Games List</h2>
+    <h2>Game List</h2>
+
+    <div v-for="game in games" @click="selectGame(game)" style="cursor: pointer;">
+        <h3>{{ game.title }}</h3>
+        <p>{{ game.developer.name }}</p>
+        <img :src="game.poster_url" style="max-height: 100px">
+    </div>
+
 </template>
